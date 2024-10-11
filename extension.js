33aaad1a40ef1f77ff236446af05473ea9919e49
tonyxqing <tonyqing2022@gmail.com>
2024-10-10 19:46:37 -0500

    updating messages. configuring enable and disable logic

--- extension.js
+++ extension.js
@@ -56,96 +56,108 @@ if (!fs.existsSync(htmlFile)) {
     "workbench.esm.html"
   );
 }
+
+const removePatch = async () => {
+  let html = await fs.promises.readFile(htmlFile, "utf-8");
+  if (!patchAlreadyEnabled(html)) {
+    vscode.window.showInformationMessage(msg.already_disabled);
+    return;
+  }
+  html = clearExistingPatches(html);
+  html = html.replace(
+    /<!-- !! (<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>) !! -->/,
+    "$1"
+  );
+  try {
+    await fs.promises.writeFile(htmlFile, html, "utf-8");
+    enabledRestart();
+  } catch (e) {
+    vscode.window.showInformationMessage(msg.admin);
+  }
+};
+const addPatch = async () => {
+  let html = await fs.promises.readFile(htmlFile, "utf-8");
+  console.log("hello is this already enabmed", patchAlreadyEnabled(html));
+  if (patchAlreadyEnabled(html)) {
+    vscode.window.showInformationMessage(msg.already_enabled);
+    return;
+  }
+  let injectHTML = "";
+  const customCode = [
+    `file:///${__dirname}/css/custom-vscode.css`,
+    `file:///${__dirname}/js/vscode-script.js`,
+  ];
+  for (const url of customCode) {
+    let parsed = new Url.URL(url);
+    const ext = path.extname(parsed.pathname);
+    try {
+      const fp = Url.fileURLToPath(url);
+      const fetched = await fs.promises.readFile(fp);
+      if (ext === ".css") {
+        injectHTML += `<style>${fetched}</style>`;
+      } else if (ext === ".js") {
+        injectHTML += `<script>${fetched}</script>`;
+      } else {
+        console.log(`Unsupported extension type: ${ext}`);
+      }
+    } catch (e) {
+      console.error(e);
+      vscode.window.showWarningMessage(msg.cannotLoad(url));
+    }
+  }
+  html = html.replace(
+    /<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>/,
+    "<!-- !! $& !! -->"
+  );
+
+  let indicatorJsPath;
+  let ext = vscode.extensions.getExtension("vscode-ui-tweaks");
+  console.log(ext);
+
+  if (ext && ext.extensionPath) {
+    indicatorJsPath = path.resolve(ext.extensionPath, "src/statusbar.js");
+  } else {
+    indicatorJsPath = path.resolve(__dirname, "statusbar.js");
+  }
+  const indicatorJsContent = await fs.promises.readFile(
+    indicatorJsPath,
+    "utf-8"
+  );
+  const indicatorJS = `<script>${indicatorJsContent}</script>`;
+
+  html = html.replace(
+    /(<\/html>)/,
+    "<!-- !! VSCODE-UI-TWEAK-START !! -->\n" +
+      indicatorJS +
+      injectHTML +
+      "<!-- !! VSCODE-UI-TWEAK-END !! -->\n</html>"
+  );
+  try {
+    await fs.promises.writeFile(htmlFile, html, "utf-8");
+    enabledRestart();
+  } catch (e) {
+    vscode.window.showInformationMessage(msg.admin);
+  }
+};
 function activate(context) {
   // patch html when extension activates
-  (async () => {
-    let html = await fs.promises.readFile(htmlFile, "utf-8");
-    console.log("hello is this already enabmed", patchAlreadyEnabled(html))
-    if (!patchAlreadyEnabled(html)) {
-      let injectHTML = "";
-      const customCode = [
-        `file:///${__dirname}/css/custom-vscode.css`,
-        `file:///${__dirname}/js/vscode-script.js`,
-      ];
-      for (const url of customCode) {
-        let parsed = new Url.URL(url);
-        const ext = path.extname(parsed.pathname);
-        try {
-          const fp = Url.fileURLToPath(url);
-          const fetched = await fs.promises.readFile(fp);
-          if (ext === ".css") {
-            injectHTML += `<style>${fetched}</style>`;
-          } else if (ext === ".js") {
-            injectHTML += `<script>${fetched}</script>`;
-          } else {
-            console.log(`Unsupported extension type: ${ext}`);
-          }
-        } catch (e) {
-          console.error(e);
-          vscode.window.showWarningMessage(msg.cannotLoad(url));
-        }
-      }
-      html = html.replace(
-        /<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>/,
-        "<!-- !! $& !! -->"
-      );
+  let enable = vscode.commands.registerCommand(
+    "vscode-ui-tweaks.addPatch",
+    addPatch
+  );
 
-      let indicatorJS = await getIndicatorJs();
+  context.subscriptions.push(enable);
 
-      html = html.replace(
-        /(<\/html>)/,
-        "<!-- !! VSCODE-UI-TWEAK-START !! -->\n" +
-          indicatorJS +
-          injectHTML +
-          "<!-- !! VSCODE-UI-TWEAK-END !! -->\n</html>"
-      );
-      try {
-        await fs.promises.writeFile(htmlFile, html, "utf-8");
-      } catch (e) {
-        disabledRestart();
-      }
-      enabledRestart();
-    } else {
-      console.log("Extension is already enabled and active")
-    }
-  })();
+  let disable = vscode.commands.registerCommand(
+    "vscode-ui-tweaks.removePatch",
+    removePatch
+  );
 
-  async function getIndicatorJs() {
-    let indicatorJsPath;
-    let ext = vscode.extensions.getExtension("be5invis.vscode-custom-css");
-    if (ext && ext.extensionPath) {
-      indicatorJsPath = path.resolve(ext.extensionPath, "src/statusbar.js");
-    } else {
-      indicatorJsPath = path.resolve(__dirname, "statusbar.js");
-    }
-    const indicatorJsContent = await fs.promises.readFile(
-      indicatorJsPath,
-      "utf-8"
-    );
-    return `<script>${indicatorJsContent}</script>`;
-  }
-
-  console.log("vscode-ui-tweaks is active!");
+  context.subscriptions.push(disable);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {
-  (async () => {
-    let html = await fs.promises.readFile(htmlFile, "utf-8");
-    html = clearExistingPatches(html);
-    html = html.replace(
-      /<!-- !! (<meta\s+http-equiv="Content-Security-Policy"[\s\S]*?\/>) !! -->/,
-      "$1"
-    );
-    try {
-      await fs.promises.writeFile(htmlFile, html, "utf-8");
-    } catch (e) {
-      disabledRestart();
-    }
-    enabledRestart();
-  })();
-  console.log("vscode-ui-tweaks is deactivated!");
-}
+function deactivate() {}
 
 module.exports = {
   activate,
--- messages.js
+++ messages.js
@@ -1,18 +1,13 @@
 exports.messages = {
-	admin: "Run VS Code with admin privileges so the changes can be applied.",
-	enabled:
-		"Custom CSS and JS enabled. Restart to take effect. " +
-		"If Code complains about it is corrupted, CLICK DON'T SHOW AGAIN. " +
-		"See README for more detail.",
-	disabled: "Custom CSS and JS disabled and reverted to default. Restart to take effect.",
-	already_disabled: "Custom CSS and JS already disabled.",
-	somethingWrong: "Something went wrong: ",
-	restartIde: "Restart Visual Studio Code",
-	notfound: "Custom CSS and JS not found.",
-	notConfigured:
-		"Custom CSS and JS path not configured. " +
-		'Please set "vscode_custom_css.imports" in your user settings.',
-	reloadAfterVersionUpgrade:
-		"Detected reloading CSS / JS after VSCode is upgraded. " + "Performing application only.",
-	cannotLoad: url => `Cannot load '${url}'. Skipping.`
+  admin: "Run VS Code with admin privileges so the changes can be applied.",
+  enabled:
+    "VSCode UI Tweaks is enabled. Restart to take effect. " +
+    "VSCode shows a message complaining the installation is corrupt,"
+	+ "click the cog icon and press 'don't show again' ",
+  disabled:
+    "VSCode UI Tweaks is disabled and reverted to default. Restart to take effect.",
+  already_enabled: "VSCode UI Tweaks is already enabled.",
+  already_disabled: "VSCode UI Tweaks is already disabled.",
+  restartIde: "Restart Visual Studio Code",
+  cannotLoad: (url) => `Cannot load '${url}'!! Check Implementation.`,
 };
